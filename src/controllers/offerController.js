deee54207b2cb2e539bd9e137452c7cf2fd4919b
Ignacio Grillo Caimary <igrilloc@pop-os.localdomain>
2023-07-21 17:13:38 -0300

    clining code

--- src/controllers/offerController.js
+++ src/controllers/offerController.js
@@ -13,7 +13,7 @@ const createOfferDB = async (
     where: { title },
   });
 
-  if (existingOffer.opportunityId) {
+  if (existingOffer) {
     return existingOffer;
   } else {
     const createdOffer = await Offer.create({
